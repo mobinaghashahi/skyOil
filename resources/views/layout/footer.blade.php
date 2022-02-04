5d9087de8dafda961261e6c320c17fb914ee68b2
Mobin <mobinaghashahi13@gmail.com>
2022-02-04 15:47:00 +0330

    create footer page

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,23 @@
+
+<div class="footer col-12">
+    <p style="font-size: large;font-weight: bolder;color: white">تماس با ما</p>
+    <div class="col-3" style="float: left;">
+        <img src="image/logo/instagram.png" width="50" height="50">
+        <p style="color: white"><b>Instagram:</b> mohandes.sho<span style="font-family: Arial">3113</span></p>
+    </div>
+    <div class="col-3" style="float: left;">
+        <img src="image/logo/email.png" width="50" height="50">
+        <p style="color: white"><b>Email:</b> mohandes.sho<span style="font-family: Arial">3113</span>@gmail.com</p>
+    </div>
+    <div class="col-3" style="float: left;">
+        <img src="image/logo/phone.png" width="50" height="50">
+        <p style="color: white"><b>Phone:</b> 09129231997</p>
+    </div>
+    <di class="col-3" style="float: left;">
+        <img src="image/logo/whatsapp.png" width="50" height="50">
+        <p style="color: white"><b>Whatsapp:</b>09910448874</p>
+    </di>
+
+</div>
+</body>
+</html>
