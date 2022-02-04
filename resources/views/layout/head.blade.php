eb25083f1e1773cf3a103720a93024ae68ba4d96
Mobin <mobinaghashahi13@gmail.com>
2022-02-04 15:44:39 +0330

    create Head

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/style.css" rel="stylesheet">
+</head>
+<body>
+<div class="col-12 nav">
+    <div class="col-4" style="float: right;width: 33.33%">
+        <ul>
+            <li>
+                <a href="" style="padding: 10px 10px 10px 7px;">
+                    <img src="/image/69524.png" width="25px" height="25px">
+                </a>
+            </li>
+            <li>
+                <a style="color:#56ba00;" href="">
+                    ثبت نام
+                </a>
+            </li>
+            <li>
+                <a href="">
+                    ورود
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="col-8" style="float: right;width: 66.66%">
+        <p style='font-size: 45px;color: white;font-family: "Bebas Neue Book";font-weight: bold;margin: 5px 5px 5px 15px;text-shadow:2px 2px #304174'>sky</p>
+    </div>
+
+</div>
