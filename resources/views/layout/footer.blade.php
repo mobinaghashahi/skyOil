bd9322277d465cf2aff660f4ae012ab150210001
Mobin <mobinaghashahi13@gmail.com>
2022-02-09 20:13:39 +0330

    change footer contact information

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -3,19 +3,19 @@
     <p style="font-size: large;font-weight: bolder;color: white">تماس با ما</p>
     <div class="col-3" style="float: left;">
         <img src="image/logo/instagram.png" width="50" height="50">
-        <p style="color: white"><b>Instagram:</b> mohandes.sho<span style="font-family: Arial">3113</span></p>
+        <p style="color: white">Taviz.Roghani.Aseman</p>
     </div>
     <div class="col-3" style="float: left;">
         <img src="image/logo/email.png" width="50" height="50">
-        <p style="color: white"><b>Email:</b> mohandes.sho<span style="font-family: Arial">3113</span>@gmail.com</p>
+        <p style="color: white">SkyOilChange@gmail.com</p>
     </div>
     <div class="col-3" style="float: left;">
         <img src="image/logo/phone.png" width="50" height="50">
-        <p style="color: white"><b>Phone:</b> 09129231997</p>
+        <p style="color: white">09127192840</p>
     </div>
     <di class="col-3" style="float: left;">
         <img src="image/logo/whatsapp.png" width="50" height="50">
-        <p style="color: white"><b>Whatsapp:</b>09910448874</p>
+        <p style="color: white">09127192840</p>
     </di>
 
 </div>
