225c9f47acb4a12eb1e63fe4bf36eac38a39ea8d
Mobin <mobinaghashahi13@gmail.com>
2022-02-09 19:29:44 +0330

    change index logo

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
 
     <div class="col-12 toppic">
-        <h1>بزرگترین مرکز خدماتی رفاهی</h1>
+        <div class="col-12" style="display: flex;justify-content: center;"><img src="image/logo.png" height="200" width="300"></div>
+        <div class="col-12"><h1>بزرگترین مرکز خدماتی رفاهی</h1></div>
+
+
     </div>
 
 @endsection
