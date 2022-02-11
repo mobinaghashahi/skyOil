3821fed34e3ef158e553b6c62ca5d9e7598d97d5
Mobin <mobinaghashahi13@gmail.com>
2022-02-11 14:41:49 +0330

    create customer migration

---
+++ database/migrations/2022_02_11_093050_create_customer.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomer extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('family');
+            $table->string('meliCode',10);
+            $table->string('carTag');
+            $table->string('phoneNumber',11);
+            $table->string('carType');
+            $table->timestamp('dateChangeOil')->nullable();
+            $table->timestamp('expirationDay')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customer');
+    }
+}
