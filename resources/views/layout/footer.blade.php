84d59fd6046d9de981327260dc3f4fe1e2340067
Mobin <mobinaghashahi13@gmail.com>
2022-02-13 20:23:35 +0330

    change footer directory and add js files

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -2,22 +2,28 @@
 <div class="footer col-12">
     <p style="font-size: large;font-weight: bolder;color: white">تماس با ما</p>
     <div class="col-3" style="float: left;">
-        <img src="image/logo/instagram.png" width="50" height="50">
+        <img src="/image/logo/instagram.png" width="50" height="50">
         <p style="color: white">Taviz.Roghani.Aseman</p>
     </div>
     <div class="col-3" style="float: left;">
-        <img src="image/logo/email.png" width="50" height="50">
+        <img src="/image/logo/email.png" width="50" height="50">
         <p style="color: white">SkyOilChange@gmail.com</p>
     </div>
     <div class="col-3" style="float: left;">
-        <img src="image/logo/phone.png" width="50" height="50">
+        <img src="/image/logo/phone.png" width="50" height="50">
         <p style="color: white">09127192840</p>
     </div>
     <di class="col-3" style="float: left;">
-        <img src="image/logo/whatsapp.png" width="50" height="50">
+        <img src="/image/logo/whatsapp.png" width="50" height="50">
         <p style="color: white">09127192840</p>
     </di>
-
 </div>
+
+
+
+
+<script src="/js/jalaali.js" type="text/javascript"></script>
+<script src="/js/jquery.Bootstrap-PersianDateTimePicker.js" type="text/javascript"></script>
+
 </body>
 </html>
