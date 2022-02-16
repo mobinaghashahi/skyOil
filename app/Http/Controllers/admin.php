83e85fe6f34c8273ad57ba3d9d16f6ffa63f3e0b
Mobin <mobinaghashahi13@gmail.com>
2022-02-16 11:30:30 +0330

    small change for validation addCustomer and added editPanelCustomer,deleteCustomer, showEditCustomer and editCustomer

--- app/Http/Controllers/admin.php
+++ app/Http/Controllers/admin.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Customer;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class admin extends Controller
@@ -14,9 +15,9 @@ class admin extends Controller
         $valid=$request->validate([
             'name'=>'required',
             'family'=>'required',
-            'meliCode'=>'required',
+            'meliCode'=>'required|max:10|min:10',
             'carTag'=>'required',
-            'phoneNumber'=>'required',
+            'phoneNumber'=>'required|max:11|min:11',
             'dateChangeOil'=>'required',
             'expirationDay'=>'required'
         ]);
@@ -31,5 +32,40 @@ class admin extends Controller
         $customer->expirationDay=$request->expirationDay;
         $customer->save();
         return redirect('admin/addCustomer')->with('msg','کاربر با موفقیت افزوده شد');
+    }
+    function editPanelCustomer(){
+        return view('editPanelCustomer',['users'=>Customer::all()]);
+    }
+    function deleteCustomer($id){
+        $customer=Customer::findOrFail($id);
+        $customer->delete();
+        return back();
+    }
+    function showEditCustomer($id){
+        return view('editCustomer',['customer'=>Customer::findOrFail($id)]);
+    }
+    function editCustomer(Request $request,$id) {
+        $valid=$request->validate([
+            'name'=>'required',
+            'family'=>'required',
+            'meliCode'=>'required|max:10|min:10',
+            'carTag'=>'required',
+            'phoneNumber'=>'required|max:11|min:11',
+            'dateChangeOil'=>'required',
+            'expirationDay'=>'required'
+        ]);
+        $customer=Customer::findOrFail($id);
+
+        $customer->update([
+            'name'=>$request->name,
+            'family'=>$request->family,
+            'meliCode'=>$request->meliCode,
+            'carTag'=>$request->carTag,
+            'phoneNumber'=>$request->phoneNumber,
+            'dateChangeOil'=>$request->dateChangeOil,
+            'expirationDay'=>$request->expirationDay,
+        ]);
+
+        return redirect('admin/editCustomer/'.$id)->with('msg','کاربر با موفقیت ویرایش شد');
     }
 }
