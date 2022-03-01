cc058d140667efe7bb413879409fe7b51a57a253
Mobin <mobinaghashahi13@gmail.com>
2022-03-01 12:20:54 +0330

    create helper function for calculat day diffrence.

--- app/helpers.php
+++ app/helpers.php
@@ -1,1 +1,49 @@
 <?php
+
+function dateExplode($date){
+    $explodeDate = explode('/', $date);
+    return $explodeDate;
+}
+function dataConvert($date)
+{
+    $explodeDate=dateExplode($date);
+    $temp = verta();
+    $temp->day = $explodeDate[2];
+    $temp->month = $explodeDate[1];
+    $temp->year = $explodeDate[0];
+    return $temp;
+}
+
+function diffDaysOfNow($date)
+{
+    $dateNow=verta();
+    return $date->diffDays($dateNow);
+}
+
+function sendSMS($number,$message){
+    $url = "https://ippanel.com/services.jspd";
+
+    $rcpt_nm = array($number);
+    $param = array
+    (
+        'uname'=>'09139638917',
+        'pass'=>'faraz1180076915',
+        'from'=>'+983000505',
+        'message'=>$message,
+        'to'=>json_encode($rcpt_nm),
+        'op'=>'send'
+    );
+
+    $handler = curl_init($url);
+    curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($handler, CURLOPT_POSTFIELDS, $param);
+    curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+    $response2 = curl_exec($handler);
+
+    $response2 = json_decode($response2);
+    $res_code = $response2[0];
+    $res_data = $response2[1];
+
+
+    echo $res_data;
+}
