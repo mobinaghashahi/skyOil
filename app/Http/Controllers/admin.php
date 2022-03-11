cb119e5f9d2c2cee275dfd6f2a5a35bfdbf3c61b
Mobin <mobinaghashahi13@gmail.com>
2022-03-11 11:38:03 +0330

    create controller for oil Change

--- app/Http/Controllers/admin.php
+++ app/Http/Controllers/admin.php
@@ -1,27 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Exports\UsersExport;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
-use App\Imports\UsersImport;
-use App\Exports\UsersExport;
-
-
-
-use App\Models\Customer;
-use App\Models\User;
-
-
 
 
 class admin extends Controller
 {
-    function showAddCostumerForm(){
+    function showAddCostumerForm()
+    {
         return view('addCustomer');
     }
-    function addCostumer(Request $request){
-        $valid=$request->validate([
-            'name'=>'required',
+
+    function addCostumer(Request $request)
+    {
+        $valid = $request->validate([
+            'name' => 'required',
             'family'=>'required',
             'meliCode'=>'required|max:10|min:10',
             'carTag'=>'required',
@@ -69,14 +65,35 @@ class admin extends Controller
             'family'=>$request->family,
             'meliCode'=>$request->meliCode,
             'carTag'=>$request->carTag,
-            'phoneNumber'=>$request->phoneNumber,
-            'dateChangeOil'=>$request->dateChangeOil,
-            'expirationDay'=>$request->expirationDay,
+            'phoneNumber' => $request->phoneNumber,
+            'dateChangeOil' => $request->dateChangeOil,
+            'expirationDay' => $request->expirationDay,
         ]);
 
-        return redirect('admin/editCustomer/'.$id)->with('msg','کاربر با موفقیت ویرایش شد');
+        return redirect('admin/editCustomer/' . $id)->with('msg', 'کاربر با موفقیت ویرایش شد');
     }
-    public function reportCustomers(Request $request){
+
+    public function reportCustomers(Request $request)
+    {
         return Excel::download(new UsersExport, 'users-collection.xlsx');
+    }
+
+    public function oilChangeView()
+    {
+        return view('oilChange');
+    }
+
+    public function oilChangeUpdate(Request $request)
+    {
+        $customer = Customer::where('meliCode', $request->meliCode)->first();
+        if ($customer == null)
+            return 1;
+        $customer->update([
+            'smsSent' => 0,
+            'dateChangeOil' => $request->dateChangeOil,
+            'expirationDay' => $request->expirationDay,
+        ]);
+        $msg='تاریخ تعویض روغن کاربر با موفقیت ویرایش شد.';
+        return redirect('admin/oilChange/')->with('msg', $msg);
     }
 }
