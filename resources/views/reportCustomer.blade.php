edb72843fd62b1d70ed682ae2eee7bc176571776
unknown <mobinaghashahi13@gmail.com>
2022-06-20 15:16:45 +0430

    edite some parameters to reportCustomer views and delete some parameters

--- resources/views/reportCustomer.blade.php
+++ resources/views/reportCustomer.blade.php
@@ -23,21 +23,26 @@ date_default_timezone_set('Iran');
         <a>مجتمع فنی و تخصصی سرویس روغن آسمان</a>
     </div>
 
-    <div class="col-12" style="padding-top: 1cm;float: right;text-align: right;direction: rtl">
-        <a style="padding-right: 10px">اطلاعات خریدار:</a>
+    <div class="col-12" style="float: right">
+        <div class="col-6" style="padding-top: 0.5cm;text-align: right;direction: rtl;float: right">
+            <a style="padding-right: 10px">نام و نام خانوادگی:  {{$customer[0]->name.' '.$customer[0]->family}}</a>
+        </div>
     </div>
-    <div class="col-4" style="padding-top: 0.5cm;text-align: right;direction: rtl">
-        <a>شماره همراه: {{$customer[0]->phoneNumber}}</a>
-    </div>
-    <div class="col-4" style="padding-top: 0.5cm;text-align: right;direction: rtl">
-        <a>شماره پلاک: {{$customer[0]->carTag}}</a>
-    </div>
-    <div class="col-4" style="padding-top: 0.5cm;text-align: right;direction: rtl">
-        <a style="padding-right: 10px">نام خودرو: {{$customer[0]->carType}}</a>
+    <div class="col-12">
+        <div class="col-4" style="padding-top: 0.5cm;text-align: right;direction: rtl">
+            <a>شماره همراه: {{$customer[0]->phoneNumber}}</a>
+        </div>
+        <div class="col-4" style="padding-top: 0.5cm;text-align: right;direction: rtl">
+            <a>شماره پلاک: {{$customer[0]->carTag}}</a>
+        </div>
+        <div class="col-4" style="padding-top: 0.5cm;text-align: right;direction: rtl">
+            <a style="padding-right: 10px">نام خودرو: {{$customer[0]->carType}}</a>
+        </div>
     </div>
 
+
     <div class="col-12"
-         style="padding-top: 2cm;float: right;text-align: right;direction: rtl;justify-content: center;display: flex">
+         style="padding-top: 1cm;float: right;text-align: right;direction: rtl;justify-content: center;display: flex">
         <table style="border-collapse: collapse;width: 95%;text-align: center">
             <tr>
                 <th>
@@ -96,19 +101,14 @@ date_default_timezone_set('Iran');
                 </th>
 
             </tr>
-            <tr>
-                <td colspan="4" style="text-align: right;padding-right: 0.5cm;direction: rtl">
-                    سرویس کار: {{$customerBuy[count($customerBuy)-1]->serviceMan}}
-                </td>
-            </tr>
         </table>
     </div>
     <div class="col-12" style="text-align: right; ">
-        <p style="padding: 5cm 0.5cm 0cm 0cm">کیلومتر 5 محور اردستان- کاشان، بعد از پلیس راه، داخل مجتمع خدمات رفاهی
+        <p style="padding: 0.5cm 0.5cm 0cm 0cm">کیلومتر 5 محور اردستان- کاشان، بعد از پلیس راه، داخل مجتمع خدمات رفاهی
             آسمان اردستان</p>
     </div>
     <div class="col-12" style="text-align: right; ">
-        <p style="padding: 2cm 0.5cm 0cm 0cm">تلفن: 09939146646</p>
+        <p style="padding: 0.5cm 0.5cm 0cm 0cm">تلفن: 09939146646</p>
     </div>
 
 </div>
