1e3562a18d7124a20090d24a5fb61a45166d56ad
unknown <mobinaghashahi13@gmail.com>
2022-07-17 15:13:14 +0430

    change title index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 
     <div class="col-12 toppic">
         <div class="col-12" style="display: flex;justify-content: center;"><img src="/image/logo.png" height="200" width="350"></div>
-        <div class="col-12"><h1>بزرگترین مرکز خدماتی رفاهی</h1></div>
+        <div class="col-12"><h1>مجتمع فنی و تخصصی <br>سرویس روغن آسمان</h1></div>
     </div>
 
 @endsection
